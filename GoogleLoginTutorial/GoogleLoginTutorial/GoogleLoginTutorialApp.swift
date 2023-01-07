90a424bf8377a757830fbb1a1d53624a9448efc3
KyungsooLee <jmtkd9196@gmail.com>
2023-01-07 19:11:34 +0900

    GoogleLoginTutorial project 생성

---
+++ GoogleLoginTutorial/GoogleLoginTutorial/GoogleLoginTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleLoginTutorialApp.swift
+//  GoogleLoginTutorial
+//
+//  Created by Kyungsoo Lee on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct GoogleLoginTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
