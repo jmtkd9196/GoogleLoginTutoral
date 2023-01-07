6429ffd00e80da2161df4caefbefbde218f81280
KyungsooLee <jmtkd9196@gmail.com>
2023-01-08 01:34:57 +0900

    info.plist 설정, firebase-ios-sdk 추가, GoogleSignIn-iOS 추가, AppDelegate 구현

--- GoogleLoginTutorial/GoogleLoginTutorial/GoogleLoginTutorialApp.swift
+++ GoogleLoginTutorial/GoogleLoginTutorial/GoogleLoginTutorialApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 @main
 struct GoogleLoginTutorialApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
