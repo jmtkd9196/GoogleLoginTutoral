2fb65d9458795533b25bf0b887eb9cdf4d36e691
KyungsooLee <jmtkd9196@gmail.com>
2023-01-08 17:31:04 +0900

    Content View 구현

--- GoogleLoginTutorial/GoogleLoginTutorial/ContentView.swift
+++ GoogleLoginTutorial/GoogleLoginTutorial/ContentView.swift
@@ -14,11 +14,31 @@ struct ContentView: View {
     
     @AppStorage("log_status") var logStatus: Bool = false
     var body: some View {
-        VStack {
-            
-            
+        if logStatus {
+            // MARK: YOUR HOME VIEW
+            DemoHome()
+        } else {
+            LoginView()
         }
-        
+    }
+    
+    @ViewBuilder
+    func DemoHome() -> some View {
+        NavigationStack {
+            Text("Logged In")
+                .navigationTitle("Multi-Login")
+                .toolbar {
+                    ToolbarItem {
+                        Button("Logout") {
+                            try? Auth.auth().signOut()
+                            GIDSignIn.sharedInstance.signOut()
+                            withAnimation(.easeInOut) {
+                                logStatus = false
+                            }
+                        }
+                    }
+                }
+        }
     }
     
     
