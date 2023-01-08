9c8345fb171fa11a35b20b583044141990453149
KyungsooLee <jmtkd9196@gmail.com>
2023-01-08 17:18:45 +0900

    logGoogleUser func 구현

--- GoogleLoginTutorial/GoogleLoginTutorial/ViewModel/LoginVM.swift
+++ GoogleLoginTutorial/GoogleLoginTutorial/ViewModel/LoginVM.swift
@@ -11,7 +11,41 @@ import GoogleSignIn
 import GoogleSignInSwift
 
 class LoginVM: ObservableObject {
+    // MARK: View Properties
+    //앱에 logStatus 저장
+    @AppStorage("log_status") var logStatus: Bool = false
+    @Published var showError: Bool = false
+    @Published var errorMessage: String = ""
     
+    // MARK: Logging Google user into Firebase
+    func logGoogleUser(user: GIDGoogleUser) {
+        Task {
+            do {
+                // 꼭 tokenString을 써서 string값으로 반환해줘야 한다. 그래야 credential에 넣을 때 인자의 type이 맞는다.
+                guard let idToken = user.idToken?.tokenString else { return }
+                let accessToken = user.accessToken.tokenString
+                let credential = OAuthProvider.credential(withProviderID: idToken, accessToken: accessToken)
+                
+                try await Auth.auth().signIn(with: credential)
+                
+                print("Success Google!")
+                await MainActor.run(body: {
+                    withAnimation(.easeInOut) {
+                        logStatus = true
+                    }
+                })
+            } catch {
+                await handleError(error: error)
+            }
+        }
+    }
+    
+    func handleError(error: Error) async {
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+        })
+    }
 }
 
 
