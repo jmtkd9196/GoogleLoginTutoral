99ab806bd94ebb65007edf302a127def418c058b
KyungsooLee <jmtkd9196@gmail.com>
2023-01-12 01:59:45 +0900

    AppDelegate 구현

---
+++ GoogleSignInWithoutFirebase/GoogleSignInWithoutFirebase/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  GoogleSignInWithoutFirebase
+//
+//  Created by Kyungsoo Lee on 2023/01/12.
+//
+
+import SwiftUI
+import GoogleSignIn
+import GoogleSignInSwift
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+    
+}
+
+
