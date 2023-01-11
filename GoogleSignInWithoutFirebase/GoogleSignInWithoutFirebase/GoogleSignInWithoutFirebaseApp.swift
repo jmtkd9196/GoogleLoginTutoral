f35e11da322b30c73650822a83082673537371eb
KyungsooLee <jmtkd9196@gmail.com>
2023-01-12 01:56:04 +0900

    Create GoogleSignInWithoutFirebase project

---
+++ GoogleSignInWithoutFirebase/GoogleSignInWithoutFirebase/GoogleSignInWithoutFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInWithoutFirebaseApp.swift
+//  GoogleSignInWithoutFirebase
+//
+//  Created by Kyungsoo Lee on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSignInWithoutFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
