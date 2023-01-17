08dcb85f83558c0d52a8a0b0820002896d64cbe7
KyungsooLee <jmtkd9196@gmail.com>
2023-01-17 20:11:08 +0900

    UserAPI 구현

--- GoogleSignInWithoutFirebase/GoogleSignInWithoutFirebase/ContentView.swift
+++ GoogleSignInWithoutFirebase/GoogleSignInWithoutFirebase/ContentView.swift
@@ -12,9 +12,38 @@ import GoogleSignIn
 struct ContentView: View {
     @StateObject var signInVM: SignInVM = .init()
     
+    //test
+    @State private var accessToken : String? = ""
+    @State private var refreshToken : String? = ""
+    @State private var email : String? = ""
+    @State private var name : String? = ""
+    @State private var imageURL : URL?
+    @State private var message: String = "API 호출 중..."
+    //test
     var body: some View {
         VStack {
             GoogleSignInButton(action: handleSignInButton)
+            
+            
+            
+            // test code
+                
+            
+            let url = URL(string: "http://hwgapp.com/join")
+            Link(destination: url!, label: {
+                HStack {
+                    Spacer()
+                    Text("직접 링크 넣기")
+                    Spacer()
+                }
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            })
+            
+            // test code
+
         }
         .padding()
     }
@@ -28,9 +57,32 @@ struct ContentView: View {
           }
           // Test code
           print("Success Google!")
-          print(result.user.idToken?.tokenString)
-          print(result.user.profile?.name)
-          print(result.user.profile?.email)
+//          print(result.user.idToken?.tokenString)
+//          print("[RefreshToken] : \(result.user.refreshToken.tokenString)")
+//          print(result.user.profile?.name)
+//          print(result.user.profile?.email)
+//          print(result.user.profile?.givenName)
+//          print(result.user.profile?.hasImage)
+//          print("[imageURL] : \(result.user.profile?.imageURL(withDimension: 320))")
+          
+          accessToken = result.user.idToken?.tokenString
+          refreshToken = result.user.refreshToken.tokenString
+          imageURL = result.user.profile?.imageURL(withDimension: 320)
+          name = result.user.profile?.name
+          email = result.user.profile?.email
+          print(accessToken)
+          print(refreshToken)
+          print(name)
+          print(email)
+          print(imageURL)
+          
+          request("http://hwgapp.com/join", "POST", ["key":"hello!"]) { (success, data) in
+              self.message = data as! String
+              
+          }
+          
+          
+          
           
           // If sign in succeeded, display the app's main content View.
         }
